05b7de562bcc77e30815ce7837dfb51c1db4146f
sunwen18 <30698014+sunwen18@users.noreply.github.com>
2017-10-28 20:18:19 +0800

    Create Permutation.java

---
+++ queues/Permutation.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+
+import java.util.Iterator;
+
+public class Permutation {
+    public static void main(String[] args){
+        int k = Integer.parseInt(args[0]) ;
+
+        RandomizedQueue <String>d = new RandomizedQueue();
+        while (!StdIn.isEmpty()){
+            d.enqueue(StdIn.readString());
+        }
+        Iterator<String> it = d.iterator();
+        for (int i=0;i<k;i++){
+            System.out.println(it.next());
+        }
+    }
+}
